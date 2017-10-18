8a2278c03b7b9d0f944fffdf4f6a5fe4d8bebc12
Alesia Kolbik <moyaalesia@mail.ru>
2017-10-18 21:27:14 +0300

    add search algorithm of primes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int number = 7;
-        while (number % 1 == 0) {
-            while (number % number == 0) {
-                System.out.println("Простое.");
+        int number = 10;
+        int deviderCount = 0;
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                deviderCount++;
             }
         }
-
-        System.out.println("Не простое.");
-
+        if (deviderCount == 2) {
+            System.out.println("Простое.");
+        } else {
+            System.out.println("Не простое.");
+        }
     }
+
+
 }
